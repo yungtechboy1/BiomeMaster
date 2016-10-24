6a4bbba3f4d751131adc34e2ceabe8b3a351a54d
NycuRO <nghergu@yahoo.com>
2016-10-24 20:10:56 +0300

    Update MainClass.java

--- src/main/java/BiomeMaster/MainClass.java
+++ src/main/java/BiomeMaster/MainClass.java
@@ -3,6 +3,12 @@ package BiomeMaster;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
 
+import cn.nukkit.level.generator.noise.Simplex;
+import cn.nukkit.math.NukkitRandom;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainClass extends PluginBase {
 
     @Override
@@ -19,4 +25,33 @@ public class MainClass extends PluginBase {
     public void onDisable() {
         this.getLogger().info(TextFormat.DARK_RED + "BiomeMaster disabled!");
     }
+    
+    public int lookup(double temperature, double rainfall) {
+        if (rainfall < 0.25) {
+            return Biome.SWAMP;
+        } else if (rainfall < 0.60) {
+            if (temperature < 0.25) {
+                return Biome.ICE_PLAINS;
+            } else if (temperature < 0.75) {
+                return Biome.PLAINS;
+            } else {
+                return Biome.DESERT;
+            }
+        } else if (rainfall < 0.80) {
+            if (temperature < 0.25) {
+                return Biome.TAIGA;
+            } else if (temperature < 0.75) {
+                return Biome.FOREST;
+            } else {
+                return Biome.BIRCH_FOREST;
+            }
+        } else {
+            if (temperature < 0.25) {
+                return Biome.MOUNTAINS;
+            } else {
+                return Biome.SMALL_MOUNTAINS;
+            }
+        }
+    }
+
 }
