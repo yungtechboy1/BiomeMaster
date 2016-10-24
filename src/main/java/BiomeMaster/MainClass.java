73fdf62378b80ddf186189e490ffd24e2c33498f
NycuRO <nghergu@yahoo.com>
2016-10-24 21:12:15 +0300

    Update MainClass.java

--- src/main/java/BiomeMaster/MainClass.java
+++ src/main/java/BiomeMaster/MainClass.java
@@ -54,13 +54,4 @@ public class MainClass extends PluginBase {
             }
         }
     }
-
-    public void recalculate() {
-        this.map = new int[64 * 64];
-        for (int i = 0; i < 64; ++i) {
-            for (int j = 0; j < 64; ++j) {
-                this.map[i + (j << 6)] = this.lookup(i / 63d, j / 63d);
-            }
-        }
-    }
 }
