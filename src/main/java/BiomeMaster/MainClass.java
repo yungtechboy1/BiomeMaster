791ec302d11e0bae43aefb897a06cc61754b5a7b
NycuRO <nghergu@yahoo.com>
2016-10-24 21:12:06 +0300

    Update MainClass.java

--- src/main/java/BiomeMaster/MainClass.java
+++ src/main/java/BiomeMaster/MainClass.java
@@ -2,6 +2,7 @@ package BiomeMaster;
 
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
+import cn.nukkit.level.generator.biome.Biome;
 
 import cn.nukkit.level.generator.noise.Simplex;
 import cn.nukkit.math.NukkitRandom;
@@ -26,21 +27,6 @@ public class MainClass extends PluginBase {
         this.getLogger().info(TextFormat.DARK_RED + "BiomeMaster disabled!");
     }
     
-    private final Biome fallback;
-    private final Simplex temperature;
-    private final Simplex rainfall;
-
-    private final Map<Integer, Biome> biomes = new HashMap<>();
-
-    private int[] map = new int[64 * 64];
-
-    public BiomeSelector(NukkitRandom random, Biome fallback) {
-        this.fallback = fallback;
-        this.temperature = new Simplex(random, 2F, 1F / 8F, 1F / 1024F);
-        this.rainfall = new Simplex(random, 2F, 1F / 8F, 1F / 1024F);
-        return true;
-    }
-
     public int lookup(double temperature, double rainfall) {
         if (rainfall < 0.25) {
             return Biome.SWAMP;
@@ -77,25 +63,4 @@ public class MainClass extends PluginBase {
             }
         }
     }
-
-    public void addBiome(Biome biome) {
-        this.biomes.put(biome.getId(), biome);
-    }
-
-    public double getTemperature(double x, double z) {
-        return (this.temperature.noise2D(x, z, true) + 1) / 2;
-    }
-
-    public double getRainfall(double x, double z) {
-        return (this.rainfall.noise2D(x, z, true) + 1) / 2;
-    }
-
-    public Biome pickBiome(double x, double z) {
-        int temperature = (int) (this.getTemperature(x, z) * 63);
-        int rainfall = (int) (this.getRainfall(x, z) * 63);
-
-        int biomeId = this.map[temperature + (rainfall << 6)];
-        return this.biomes.containsKey(biomeId) ? this.biomes.get(biomeId) : this.fallback;
-    }
-
 }
