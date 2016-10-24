2463bdbdedf8c2b07cc41d03ba7ac61ded4080f8
NycuRO <nghergu@yahoo.com>
2016-10-24 20:26:24 +0300

    Update MainClass.java

--- src/main/java/BiomeMaster/MainClass.java
+++ src/main/java/BiomeMaster/MainClass.java
@@ -26,6 +26,20 @@ public class MainClass extends PluginBase {
         this.getLogger().info(TextFormat.DARK_RED + "BiomeMaster disabled!");
     }
     
+    private final Biome fallback;
+    private final Simplex temperature;
+    private final Simplex rainfall;
+
+    private final Map<Integer, Biome> biomes = new HashMap<>();
+
+    private int[] map = new int[64 * 64];
+
+    public BiomeSelector(NukkitRandom random, Biome fallback) {
+        this.fallback = fallback;
+        this.temperature = new Simplex(random, 2F, 1F / 8F, 1F / 1024F);
+        this.rainfall = new Simplex(random, 2F, 1F / 8F, 1F / 1024F);
+    }
+
     public int lookup(double temperature, double rainfall) {
         if (rainfall < 0.25) {
             return Biome.SWAMP;
@@ -52,6 +66,35 @@ public class MainClass extends PluginBase {
                 return Biome.SMALL_MOUNTAINS;
             }
         }
+    }
+
+    public void recalculate() {
+        this.map = new int[64 * 64];
+        for (int i = 0; i < 64; ++i) {
+            for (int j = 0; j < 64; ++j) {
+                this.map[i + (j << 6)] = this.lookup(i / 63d, j / 63d);
+            }
+        }
+    }
+
+    public void addBiome(Biome biome) {
+        this.biomes.put(biome.getId(), biome);
+    }
+
+    public double getTemperature(double x, double z) {
+        return (this.temperature.noise2D(x, z, true) + 1) / 2;
+    }
+
+    public double getRainfall(double x, double z) {
+        return (this.rainfall.noise2D(x, z, true) + 1) / 2;
+    }
+
+    public Biome pickBiome(double x, double z) {
+        int temperature = (int) (this.getTemperature(x, z) * 63);
+        int rainfall = (int) (this.getRainfall(x, z) * 63);
+
+        int biomeId = this.map[temperature + (rainfall << 6)];
+        return this.biomes.containsKey(biomeId) ? this.biomes.get(biomeId) : this.fallback;
     }
 
 }
