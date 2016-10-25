482bf1d7acd405db3442f876543e20241603cc7d
NycuRO <nghergu@yahoo.com>
2016-10-25 14:19:48 +0300

    Create NormalBiome.java

---
+++ src/main/java/BiomeMaster/biomes/NormalBiome.java
@@ -0,0 +1,12 @@
+package BiomeMaster.biome;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public abstract class NormalBiome extends Biome {
+    @Override
+    public int getColor() {
+        return this.grassColor;
+    }
+}
