7a4bc360a8e274af5f0dd902aa7de1e31a2855a8
NycuRO <nghergu@yahoo.com>
2016-10-25 14:29:20 +0300

    Create IcePlainsBiome.java

---
+++ src/main/java/BiomeMaster/biomes/generation/IcePlainsBiome.java
@@ -0,0 +1,34 @@
+package BiomeMaster.biomes.generation;
+
+import cn.nukkit.block.BlockSapling;
+import cn.nukkit.level.generator.populator.PopulatorTallGrass;
+import cn.nukkit.level.generator.populator.PopulatorTree;
+
+import BiomeMaster.biomes.SnowyBiome;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class IcePlainsBiome extends SnowyBiome {
+
+    public IcePlainsBiome() {
+        super();
+        PopulatorTallGrass tallGrass = new PopulatorTallGrass();
+        tallGrass.setBaseAmount(5);
+
+        PopulatorTree trees = new PopulatorTree(BlockSapling.SPRUCE);
+        trees.setBaseAmount(1);
+        trees.setRandomAmount(1);
+
+        this.addPopulator(tallGrass);
+        this.addPopulator(trees);
+        this.setElevation(63, 74);
+        this.temperature = 0D;
+        this.rainfall = 0.5D;
+    }
+
+    public String getName() {
+        return "Ice Plains";
+    }
+}
