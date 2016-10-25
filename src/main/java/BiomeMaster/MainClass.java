e6a87360599f7bd932406f6e18288497c40d24f9
NycuRO <nghergu@yahoo.com>
2016-10-25 14:53:04 +0300

    test if work

--- src/main/java/BiomeMaster/MainClass.java
+++ src/main/java/BiomeMaster/MainClass.java
@@ -24,22 +24,6 @@ public class MainClass extends PluginBase {
     public int lookup(double temperature, double rainfall) {
         if (rainfall < 0.25) {
             return Biome.SWAMP;
-        } else if (rainfall < 0.60) {
-            if (temperature < 0.25) {
-                return Biome.ICE_PLAINS;
-            } else if (temperature < 0.75) {
-                return Biome.PLAINS;
-            } else {
-                return Biome.DESERT;
-            }
-        } else if (rainfall < 0.80) {
-            if (temperature < 0.25) {
-                return Biome.TAIGA;
-            } else if (temperature < 0.75) {
-                return Biome.FOREST;
-            } else {
-                return Biome.BIRCH_FOREST;
-            }
         } else {
             if (temperature < 0.25) {
                 return Biome.MOUNTAINS;
