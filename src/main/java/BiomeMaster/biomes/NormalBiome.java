a8e359fbe29298297a911501d97622e6ea657286
NycuRO <nghergu@yahoo.com>
2016-10-25 14:59:31 +0300

    Update NormalBiome.java

--- src/main/java/BiomeMaster/biomes/NormalBiome.java
+++ src/main/java/BiomeMaster/biomes/NormalBiome.java
@@ -1,12 +1,12 @@
 package BiomeMaster.biomes;
 
-import BiomeMaster.Biome;
+import BiomeMaster.Biomes;
 
 /**
  * author: MagicDroidX
  * Nukkit Project
  */
-public abstract class NormalBiome extends Biome {
+public abstract class NormalBiome extends Biomes {
     @Override
     public int getColor() {
         return this.grassColor;
