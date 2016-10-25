cc48fc9baf5cd14ca7a88ecd36f4c4db42da1b3b
NycuRO <nghergu@yahoo.com>
2016-10-25 14:31:56 +0300

    Create RiverBiome.java

---
+++ src/main/java/BiomeMaster/biomes/generation/RiverBiome.java
@@ -0,0 +1,45 @@
+package BiomeMaster.biomes.generation;
+
+import cn.nukkit.level.generator.populator.PopulatorGrass;
+import cn.nukkit.level.generator.populator.PopulatorSugarcane;
+import cn.nukkit.level.generator.populator.PopulatorTallGrass;
+import cn.nukkit.level.generator.populator.PopulatorTallSugarcane;
+
+import BiomeMaster.biomes.generation.WateryBiome;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class RiverBiome extends WateryBiome {
+
+    public RiverBiome() {
+        super();
+
+        PopulatorSugarcane sugarcane = new PopulatorSugarcane();
+        sugarcane.setBaseAmount(6);
+        PopulatorTallSugarcane tallSugarcane = new PopulatorTallSugarcane();
+        tallSugarcane.setBaseAmount(60);
+
+        PopulatorGrass grass = new PopulatorGrass();
+        grass.setBaseAmount(30);
+        this.addPopulator(grass);
+
+        PopulatorTallGrass tallGrass = new PopulatorTallGrass();
+        tallGrass.setBaseAmount(5);
+
+        this.addPopulator(tallGrass);
+        this.addPopulator(sugarcane);
+        this.addPopulator(tallSugarcane);
+
+        this.setElevation(58, 62);
+
+        this.temperature = 0.5;
+        this.rainfall = 0.7;
+    }
+
+    @Override
+    public String getName() {
+        return "River";
+    }
+}
