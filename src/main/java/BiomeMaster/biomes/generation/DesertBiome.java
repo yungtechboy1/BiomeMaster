73a847b883c82bb76c2130f56e57dac2b54496c7
NycuRO <nghergu@yahoo.com>
2016-10-25 14:26:41 +0300

    Create DesertBiome.java

---
+++ src/main/java/BiomeMaster/biomes/generation/DesertBiome.java
@@ -0,0 +1,21 @@
+package BiomeMaster.biomes.generation;
+
+import BiomeMaster.biomes.SandyBiome;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class DesertBiome extends SandyBiome {
+    public DesertBiome() {
+        super();
+        this.setElevation(63, 74);
+        this.temperature = 2;
+        this.rainfall = 0;
+    }
+
+    @Override
+    public String getName() {
+        return "Desert";
+    }
+}
