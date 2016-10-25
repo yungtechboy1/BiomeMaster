9b4557a320d8e810f3402e82158132a6ae5c9c88
NycuRO <nghergu@yahoo.com>
2016-10-25 14:50:30 +0300

    Update Biome.java

--- src/main/java/BiomeMaster/Biome.java
+++ src/main/java/BiomeMaster/Biome.java
@@ -9,6 +9,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import BiomeMaster.biomes.generation.OceanBiome;
+import BiomeMaster.biomes.generation.PlainBiome;
+import BiomeMaster.biomes.generation.DesertBiome;
+import BiomeMaster.biomes.generation.ForestBiome;
+import BiomeMaster.biomes.generation.TaigaBiome;
+import BiomeMaster.biomes.generation.SwampBiome;
+import BiomeMaster.biomes.generation.RiverBiome;
+import BiomeMaster.biomes.generation.IcePlainsBiome;
+import BiomeMaster.biomes.generation.BeachBiome;
+
 /**
  * author: MagicDroidX
  * Nukkit Project
@@ -20,23 +30,13 @@ public abstract class Biome {
     public static final int OCEAN = 0;
     public static final int PLAINS = 1;
     public static final int DESERT = 2;
-    public static final int MOUNTAINS = 3;
     public static final int FOREST = 4;
     public static final int TAIGA = 5;
     public static final int SWAMP = 6;
     public static final int RIVER = 7;
-
     public static final int ICE_PLAINS = 12;
-
-
     public static final int BEACH = 16;
-
-
-    public static final int SMALL_MOUNTAINS = 20;
-
-
     public static final int BIRCH_FOREST = 27;
-
 
     public static final int MAX_BIOMES = 256;
 
@@ -66,15 +66,12 @@ public abstract class Biome {
         register(OCEAN, new OceanBiome());
         register(PLAINS, new PlainBiome());
         register(DESERT, new DesertBiome());
-        register(MOUNTAINS, new MountainsBiome());
         register(FOREST, new ForestBiome());
         register(TAIGA, new TaigaBiome());
         register(SWAMP, new SwampBiome());
         register(RIVER, new RiverBiome());
         register(ICE_PLAINS, new IcePlainsBiome());
-        register(SMALL_MOUNTAINS, new SmallMountainsBiome());
         register(BIRCH_FOREST, new ForestBiome(ForestBiome.TYPE_BIRCH));
-        
         register(BEACH, new BeachBiome());
     }
 
