f50d13886639ab8c4ba8ab38656b16a68af30587
NycuRO <nghergu@yahoo.com>
2016-10-28 15:46:36 +0300

    Create SwamplandBiome.java

---
+++ src/main/java/BiomeMaster/biome/SwamplandBiome.java
@@ -0,0 +1,48 @@
+package BiomeMaster.biome;
+
+import cn.nukkit.block.Block;
+import cn.nukkit.block.BlockFlower;
+import cn.nukkit.block.BlockSapling;
+import cn.nukkit.level.generator.populator.PopulatorFlower;
+import cn.nukkit.level.generator.populator.PopulatorLilyPad;
+import BiomeMaster.populator.SwampTreePopulator;
+
+public class SwamplandBiome extends GrassyBiome {
+
+    public SwamplandBiome() {
+        super();
+
+        PopulatorLilyPad lilypad = new PopulatorLilyPad();
+        lilypad.setBaseAmount(4);
+
+        PopulatorTree trees = new SwampTreePopulator();
+        trees.setBaseAmount(2);
+
+        PopulatorFlower flower = new PopulatorFlower();
+        flower.setBaseAmount(2);
+        flower.addType(Block.RED_FLOWER, BlockFlower.TYPE_BLUE_ORCHID);
+
+        this.addPopulator(trees);
+        this.addPopulator(flower);
+        this.addPopulator(lilypad);
+
+        this.setElevation(62, 63);
+
+        this.temperature = 0.8f;
+        this.rainfall = 0.9f;
+    }
+
+    @Override
+    public String getName() {
+        return "Swamp";
+    }
+
+    @Override
+    public int getColor() { // When this temperature goes below -0.1, a lush green color is used, 0x4C763C, otherwise it is set to a sickly brown, 0x6A7039. In addition, the color of the water in swamplands is always multiplied by a greenish tinge, 0xE0FFAE.
+        return 0x4C763C || 0x6A7039 || 0xE0FFAE;
+    }
+    
+    public void initBiome() {
+        register(NormalGenerator.SWAMP, this);
+    }
+}
