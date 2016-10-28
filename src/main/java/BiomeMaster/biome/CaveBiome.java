04ea81bd58fa9c6707966dc90455607c93a5bc1c
NycuRO <nghergu@yahoo.com>
2016-10-28 22:57:14 +0300

    Create CaveBiome.java

---
+++ src/main/java/BiomeMaster/biome/CaveBiome.java
@@ -0,0 +1,13 @@
+package BiomeMaster.biome;
+
+/**
+ * Nukkit Project
+ * Author: MagicDroidX
+ */
+public interface CaveBiome {
+    int getStoneBlock();
+
+    int getSurfaceBlock();
+
+    int getGroundBlock();
+}
