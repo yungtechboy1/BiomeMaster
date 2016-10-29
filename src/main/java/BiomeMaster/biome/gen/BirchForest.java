85131b3b982d5cd570412d9ba7a0c191a54056d8
NycuRO <nghergu@yahoo.com>
2016-10-29 11:21:27 +0300

    It's not correct to create separatly Forests ,desc
    It's not correct to create separatly forests : Birch ,normal and Roofed Forest because NMS. I'm using that for register all biomes from plugin,not from plugin :) Probably it's ok that.

---
+++ src/main/java/BiomeMaster/biome/gen/BirchForest.java
@@ -0,0 +1,42 @@
+package BiomeMaster.biome.gen;
+
+import cn.nukkit.block.BlockSapling;
+import cn.nukkit.level.generator.populator.PopulatorGrass;
+import cn.nukkit.level.generator.populator.PopulatorTallGrass;
+import cn.nukkit.level.generator.populator.PopulatorTree;
+import BiomeMaster.biome.GrassyBiome;
+import BiomeMaster.generator.NormalGenerator;
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+
+public class BirchForestBiome extends GrassyBiome {
+
+    public ForestBiome() {
+        super();
+
+        PopulatorTree trees = new PopulatorTree(BlockSapling.BIRCH);
+        trees.setBaseAmount(5);
+        this.addPopulator(trees);
+
+        PopulatorGrass grass = new PopulatorGrass();
+        grass.setBaseAmount(30);
+        this.addPopulator(grass);
+
+        PopulatorTallGrass tallGrass = new PopulatorTallGrass();
+        tallGrass.setBaseAmount(3);
+
+        this.addPopulator(tallGrass);
+
+        this.setElevation(63, 81);
+
+        this.temperature = 0.6f;
+        this.rainfall = 0.6f;
+    }
+
+    @Override
+    public String getName() {
+        return "Birch Forest";
+    }
+}
