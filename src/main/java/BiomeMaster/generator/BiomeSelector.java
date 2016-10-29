ef3c89c3a1264d0ef8a1dc4e1abc08d5f8edb6e6
NycuRO <nghergu@yahoo.com>
2016-10-29 19:20:49 +0300

    now fix generation

--- src/main/java/BiomeMaster/generator/BiomeSelector.java
+++ src/main/java/BiomeMaster/generator/BiomeSelector.java
@@ -17,56 +17,68 @@ public class BiomeSelector {
 
     private final Map<Integer, Biome> biomes = new HashMap<>();
 
-    private int[] map = new int[4096];
+    private int[] map = new int[64 * 64];
 
     public BiomeSelector(NukkitRandom random, Biome fallback) {
         this.fallback = fallback;
-        this.temperature = new Simplex(random, 2.0f, 0.125f, 9.765625E-4f);
-        this.rainfall = new Simplex(random, 2.0f, 0.125f, 9.765625E-4f);
+        this.temperature = new Simplex(random, 2F, 1F / 8F, 1F / 1024F);
+        this.rainfall = new Simplex(random, 2F, 1F / 8F, 1F / 1024F);
     }
 
     public int lookup(double temperature, double rainfall) {
-        if (rainfall < 0.25) {
-            if (temperature < 0.9) {
+        if (temperature < 0.8f) {
+            if (rainfall < 0.4f) {
+                return NormalGenerator.PLAINS;
+            } else if (rainfall < 0.9f) {
                 return NormalGenerator.SWAMP;
             } else {
-                return NormalGenerator.SAVANNA;
+                if (temperature < 2.0f) {
+                    if (rainfall == 0.0f) {
+                        return NormalGenerator.DESERT;
+                    }
+                }
             }
-        } else if (rainfall < 0.60) {
-            if (temperature < 0.25) {
-                return NormalGenerator.ICE_PLAINS;
-            } else if (temperature < 0.75) {
-                return NormalGenerator.PLAINS;
-            } else {
-                return NormalGenerator.DESERT;
-            }
-        } else if (rainfall < 0.80) {
-            if (temperature < 0.25) {
-                return NormalGenerator.TAIGA;
-            } else if (temperature < 0.75) {
-                return NormalGenerator.FOREST;
-            } else {
-                return NormalGenerator.BIRCH_FOREST;
-            }
-        } else {
-            if (temperature < 0.25) {
-                return Biome.MOUNTAINS;
-            } else if (temperature < 0.5) {
-                return NormalGenerator.ROOFED_FOREST_M;
-            } else if (temperature < 0.8) {
-                return NormalGenerator.ROOFED_FOREST;
-            } else if (temperature < 1.3) {
+        } else if (temperature < 1.2f) {
+            if (rainfall < 0.9f) {
                 return NormalGenerator.JUNGLE;
             } else {
-                return Biome.SMALL_MOUNTAINS;
+                if (rainfall == 0.0f) {
+                    return NormalGenerator.SAVANNA;
+                }
             }
+        } else if (temperature < 0.05f) {
+            if (rainfall < 0.8f) {
+                return NormalGenerator.TAIGA;
+            }
+        } else if (temperature < 0.6f) {
+            if (rainfall < 0.6f) {
+                return NormalGenerator.BIRCH_FOREST;
+            }
+        } else if (temperature < 0.9f) {
+            if (rainfall < 1.0f) {
+                return NormalGenerator.MUSHROOM_ISLAND;
+            }
+        } else if (temperature == 0.0f) {
+            if (rainfall = 0.5f) {
+                return NormalGenerator.ICE_PLAINS;
+            }
+        } else if (temperature < 0.7f) {
+            if (rainfall < 0.8f) {
+                return NormalGenerator.FOREST;
+            } else {
+                return NormalGenerator.ROOFED_FOREST;
+            }
+        } else {
+            return NormalGenerator.ROOFED_FOREST_M;
         }
+    return NormalGenerator.OCEAN;
     }
+    
     public void recalculate() {
-        this.map = new int[4096];
+        this.map = new int[64 * 64];
         for(int i = 0; i < 64; ++i) {
             for(int j = 0; j < 64; ++j) {
-                this.map[i + (j << 6)] = this.lookup((double)i / 63.0f, (double)j / 63.0f);
+                this.map[i + (j << 6)] = this.lookup(i / 63d, j / 63d);
             }
         }
     }
@@ -76,20 +88,20 @@ public class BiomeSelector {
     }
 
     public double getTemperature(double x, double z) {
-        return (this.temperature.noise2D(x, z, true) + 1.0f) / 2.0f;
+        return (this.temperature.noise2D(x, z, true) + 1) / 2;
     }
 
     public double getRainfall(double x, double z) {
-        return (this.rainfall.noise2D(x, z, true) + 1.0f) / 2.0f;
+        return (this.rainfall.noise2D(x, z, true) + 1) / 2;
     }
 
     public Biome pickBiome(double x, double z) {
-        int temperature = (int)(this.getTemperature(x, z) * 63.0f);
-        int rainfall = (int)(this.getRainfall(x, z) * 63.0f);
-
+        int temperature = (int) (this.getTemperature(x, z) * 63);
+        int rainfall = (int) (this.getRainfall(x, z) * 63);
+        
         //System.out.println("temperature: "+temperature+"     rainfall: "+rainfall);
 
         int biomeId = this.map[temperature + (rainfall << 6)];
-        return this.biomes.containsKey(Integer.valueOf(biomeId))?(Biome)this.biomes.get(Integer.valueOf(biomeId)):this.fallback;
+        return this.biomes.containsKey(biomeId) ? this.biomes.get(biomeId) : this.fallback;
     }
 }
