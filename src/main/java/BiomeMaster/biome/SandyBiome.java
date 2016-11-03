7f96c6ff9c85ac2f0330b4a20a66de24103549bf
NycuRO <nghergu@yahoo.com>
2016-11-03 21:22:57 +0200

    Update SandyBiome.java

--- src/main/java/BiomeMaster/biome/SandyBiome.java
+++ src/main/java/BiomeMaster/biome/SandyBiome.java
@@ -3,24 +3,13 @@ package BiomeMaster.biome;
 import cn.nukkit.block.Block;
 import cn.nukkit.block.BlockSand;
 import cn.nukkit.block.BlockSandstone;
-import cn.nukkit.level.generator.populator.PopulatorCactus;
-import cn.nukkit.level.generator.populator.PopulatorDeadBush;
 
 /**
  * author: MagicDroidX
  * Nukkit Project
  */
-public abstract class SandyBiome extends NormalBiome implements CaveBiome{
+public abstract class SandyBiome extends NormalBiome implements CaveBiome {
     public SandyBiome() {
-
-        PopulatorCactus cactus = new PopulatorCactus();
-        cactus.setBaseAmount(2);
-
-        PopulatorDeadBush deadbush = new PopulatorDeadBush();
-        deadbush.setBaseAmount(2);
-
-        this.addPopulator(cactus);
-        this.addPopulator(deadbush);
 
         this.setGroundCover(new Block[]{
                 new BlockSand(),
@@ -29,20 +18,5 @@ public abstract class SandyBiome extends NormalBiome implements CaveBiome{
                 new BlockSandstone(),
                 new BlockSandstone()
         });
-    }
-
-    @Override
-    public int getSurfaceBlock() {
-        return Block.SAND;
-    }
-
-    @Override
-    public int getGroundBlock() {
-        return Block.SAND;
-    }
-
-    @Override
-    public int getStoneBlock() {
-        return Block.SANDSTONE;
     }
 }
