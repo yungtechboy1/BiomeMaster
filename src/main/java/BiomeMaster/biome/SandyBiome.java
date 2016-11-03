c32070f5d7cdd288117740429c951b845ecabda6
NycuRO <nghergu@yahoo.com>
2016-11-03 21:47:35 +0200

    Update SandyBiome.java

--- src/main/java/BiomeMaster/biome/SandyBiome.java
+++ src/main/java/BiomeMaster/biome/SandyBiome.java
@@ -11,13 +11,14 @@ import cn.nukkit.block.BlockSandstone;
 public abstract class SandyBiome extends NormalBiome implements CaveBiome {
     public SandyBiome() {
 
-        this.setGroundCover(new Block[]{
-                new BlockSand(),
-                new BlockSand(),
-                new BlockSandstone(),
-                new BlockSandstone(),
-                new BlockSandstone()
+    this.setGroundCover(new Block[]{
+        new BlockSand(),
+        new BlockSand(),
+        new BlockSandstone(),
+        new BlockSandstone(),
+        new BlockSandstone()
         });
+    }
         
     @Override
     public int getSurfaceBlock() {
