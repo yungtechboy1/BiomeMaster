eb9d84aed4e6b7b2905691a8a1f1276c94e2b3ae
NycuRO <nghergu@yahoo.com>
2016-11-03 21:27:28 +0200

    Update DesertBiome.java

--- src/main/java/BiomeMaster/biome/gen/DesertBiome.java
+++ src/main/java/BiomeMaster/biome/gen/DesertBiome.java
@@ -2,6 +2,8 @@ package BiomeMaster.biome.gen;
 
 import BiomeMaster.biome.SandyBiome;
 import BiomeMaster.generator.NormalGenerator;
+import cn.nukkit.level.generator.populator.PopulatorCactus;
+import cn.nukkit.level.generator.populator.PopulatorDeadBush;
 
 /**
  * author: MagicDroidX
@@ -10,7 +12,18 @@ import BiomeMaster.generator.NormalGenerator;
 public class DesertBiome extends SandyBiome {
     public DesertBiome() {
         super();
+        
+        PopulatorCactus cactus = new PopulatorCactus();
+        cactus.setRandomAmount(10);
+
+        PopulatorDeadBush deadbush = new PopulatorDeadBush();
+        deadbush.setRandomAmount(2);
+
+        this.addPopulator(cactus);
+        this.addPopulator(deadbush);
+        
         this.setElevation(63, 69);
+        
         this.temperature = 2.0f;
         this.rainfall = 0.0f;
     }
@@ -20,6 +33,21 @@ public class DesertBiome extends SandyBiome {
         return "Desert";
     }
     
+    @Override
+    public int getSurfaceBlock() {
+        return Block.SAND;
+    }
+
+    @Override
+    public int getGroundBlock() {
+        return Block.SAND;
+    }
+
+    @Override
+    public int getStoneBlock() {
+        return Block.SANDSTONE;
+    }
+
     public void initBiome() {
         register(NormalGenerator.DESERT, this);
     }
