4a0516ca1aba8881cc14ee28b2148d6a5f880a87
NycuRO <nghergu@yahoo.com>
2016-11-07 23:24:29 +0200

    Update SwampTree.java

--- src/main/java/BiomeMaster/object/tree/SwampTree.java
+++ src/main/java/BiomeMaster/object/tree/SwampTree.java
@@ -42,7 +42,7 @@ public class SwampTree extends TreeGenerator {
                     for (int i1 = position.getZ() - k; i1 <= position.getZ() + k && flag; ++i1) {
                         if (j >= 0 && j < 256) {
                             pos2.setComponents(l, j, i1);
-                            int block = worldIn.getBlockIdAt(pos2.x, pos2.y, pos2.z);
+                            block = worldIn.getBlockIdAt(pos2.x, pos2.y, pos2.z);
                             if (pos2 != Block.AIR && pos2 != Block.LEAVES) {
                                 if (block != Block.WATER && block != Block.STILL_WATER) {
                                     flag = false;
