b3b51b6c6ca790e397de16b0034f743c4ba9a771
NycuRO <nghergu@yahoo.com>
2016-11-19 12:12:08 +0200

    Create package-info.java

---
+++ src/main/java/BiomeMaster/block/material/package-info.java
@@ -0,0 +1,6 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package BiomeMaster.block.material;
+
+import mcp.MethodsReturnNonnullByDefault;
+import javax.annotation.ParametersAreNonnullByDefault;
